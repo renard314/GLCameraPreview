a99ff69449bccdacba136487b3d92fa760bedfda
Renard Wellnitz <Renard.Wellnitz@googlemail.com>
2012-04-29 12:28:38 +0200

    added obj to gitignore

--- obj/local/armeabi-v7a/objs/jpeg/jidctfst.o.d
+++
@@ -1,5 +0,0 @@
-/Users/renard/Desktop/devel/workspace/GLCameraPreview/obj/local/armeabi-v7a/objs/jpeg/jidctfst.o: \
- /Users/renard/Desktop/devel/libjpeg/jidctfst.S \
- /Users/renard/Desktop/devel/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/machine/cpu-features.h
-
-/Users/renard/Desktop/devel/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/machine/cpu-features.h:
--- obj/local/armeabi-v7a/objs/lept/Users/renard/Desktop/devel/leptonica-1.68/src/leptwin.o.d
+++
@@ -1,2 +0,0 @@
-/Users/renard/Desktop/devel/workspace/GLCameraPreview/obj/local/armeabi-v7a/objs/lept//Users/renard/Desktop/devel/leptonica-1.68/src/leptwin.o: \
- //Users/renard/Desktop/devel/leptonica-1.68/src/leptwin.c
